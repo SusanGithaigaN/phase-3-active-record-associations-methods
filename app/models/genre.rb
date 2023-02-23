0c79c877ea5501ff5399e8cbf14cf5cf80119410
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-02-23 14:38:16 +0300

    Completed assignment

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    
+    # select * artists, map their names
+    Artist.all.map(&:name)
   end
 end
